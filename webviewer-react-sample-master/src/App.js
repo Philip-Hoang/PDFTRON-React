6d0233800f4e02946d7f5c909747dc94a4a66c84
philip-hoang <phoang0910@gmail.com>
2020-07-19 23:48:50 -0700

    added test button

--- webviewer-react-sample-master/src/App.js
+++ webviewer-react-sample-master/src/App.js
@@ -1,35 +1,54 @@
-import React, { useRef, useEffect } from 'react';
-import WebViewer from '@pdftron/webviewer';
-import './App.css';
+import React, { useRef, useEffect } from "react";
+import WebViewer from "@pdftron/webviewer";
+import "./App.css";
 
 const App = () => {
   const viewer = useRef(null);
 
-  // if using a class, equivalent of componentDidMount 
+  // if using a class, equivalent of componentDidMount
   useEffect(() => {
     WebViewer(
       {
-        path: '/webviewer/lib',
-        initialDoc: '/files/pdftron_about.pdf',
+        path: "/webviewer/lib",
+        initialDoc: "/files/pdftron_about.pdf",
+        ui: "beta",
       },
-      viewer.current,
+      viewer.current
     ).then((instance) => {
-      const { docViewer, Annotations } = instance;
-      const annotManager = docViewer.getAnnotationManager();
+      const { docViewer } = instance;
 
-      docViewer.on('documentLoaded', () => {
-        const rectangleAnnot = new Annotations.RectangleAnnotation();
-        rectangleAnnot.PageNumber = 1;
-        // values are in page coordinates with (0, 0) in the top left
-        rectangleAnnot.X = 100;
-        rectangleAnnot.Y = 150;
-        rectangleAnnot.Width = 200;
-        rectangleAnnot.Height = 50;
-        rectangleAnnot.Author = annotManager.getCurrentUser();
+      docViewer.on("documentLoaded", () => {
+        //
+        instance.setHeaderItems((header) => {
+          header.push(
+            {
+              type: "actionButton",
+              img: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                    <path d="m0 0h24v24h-24z" fill="none"/>
+                    <path d="m16.9739609 5.6099999h-2.5857864v-2h4 2v6h-2v-2.58578644l-13.36396104 13.36396104-1.41421356-1.4142136z"/>
+                  </svg>`,
+              onClick: () => {
+                // save the annotations
+                console.log("i made a button!");
+              },
+            },
+            {
+              type: "toggleElementButton",
+              img: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                    <path d="m0 0h24v24h-24z" fill="none"/>
+                    <path d="m16.9739609 5.6099999h-2.5857864v-2h4 2v6h-2v-2.58578644l-13.36396104 13.36396104-1.41421356-1.4142136z"/>
+                  </svg>`,
+              button: "notes panel",
+              element: "leftPanel",
+              dataElement: "leftPanelButton",
+              title: "Notes Panel",
+            }
+          );
+        });
 
-        annotManager.addAnnotation(rectangleAnnot);
-        // need to draw the annotation otherwise it won't show up until the page is refreshed
-        annotManager.redrawAnnotation(rectangleAnnot);
+        //
+
+        //
       });
     });
   }, []);
